71a8b9b3f50525edb67fbe52de46405015360425
rajeshchauhan1426 <codedemon1426@gmail.com>
2024-09-06 22:34:47 +0530

    new

--- servernextjs/app/page.tsx
+++ servernextjs/app/page.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 async function getUserDetails() {
-  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  const response = await axios.get("http://localhost:3000/api/user")
 	return response.data;
 }
 
@@ -9,10 +9,18 @@ export default async function Home() {
   const userData = await getUserDetails();
 
   return (
-    <div>
-      {userData.email}
-      {userData.name}
-      {userData.address.city}
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+               
+                
+            </div>
+        </div>
     </div>
   );
 }
